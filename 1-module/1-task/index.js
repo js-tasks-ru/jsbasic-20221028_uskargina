6f5d6abb79116943f6294940c3d91c95af9b8d9e
IuliannaK <uskargina@gmail.com>
2022-11-18 19:20:39 +0300

    Task 2

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  //  n! = n * (n - 1) * (n - 2) * ...*1
+  let res = 0;
+  for (let i = 0; i <= n; i++) {
+    if (i === 1 || i === 0) {
+      res = 1;
+    } else {
+      res = res * i;
+    }
+  }
+  return res;
 }
